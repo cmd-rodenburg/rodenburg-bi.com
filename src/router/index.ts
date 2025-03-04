f3300feb1fbd2a416b1d3652185fe6b6a5d5395c
Anne Rodenburg <rodenburg.bi@gmail.com>
2025-03-04 08:26:11 +0100

    Added Posthog for pagecaptures

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,37 +1,43 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import type { RouteRecordRaw } from 'vue-router'
-
-const routes: RouteRecordRaw[] = [
-  {
-    path: '/',
-    name: 'Home',
-    component: () => import('../pages/Home.vue'), // Lazy load Home
-    meta: {
-      title: 'Home'
-    }
-  },
-  {
-    path: '/project-experience',
-    name: 'ProjectExperience',
-    component: () => import('../pages/ProjectExperience.vue')
-  },
-  {
-    path: '/test-page',
-    name: 'TestPage',
-    component: () => import('../pages/_testpage.vue')
-  },
-  {
-    path: '/meet-Anne',
-    name: 'MeetAnne',
-    component: () => import('../pages/BookMeeting.vue')
-  }
-]
-
-
+import { usePostHog } from '../composables/usePostHog'
 
 const router = createRouter({
   history: createWebHistory(),
-  routes
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: () => import('../pages/Home.vue'), // Lazy load Home
+      meta: {
+        title: 'Home'
+      }
+    },
+    {
+      path: '/project-experience',
+      name: 'ProjectExperience',
+      component: () => import('../pages/ProjectExperience.vue')
+    },
+    {
+      path: '/test-page',
+      name: 'TestPage',
+      component: () => import('../pages/_testpage.vue')
+    },
+    {
+      path: '/meet-Anne',
+      name: 'MeetAnne',
+      component: () => import('../pages/BookMeeting.vue')
+    }
+  ]
 })
+
+
+// SETUP to log page views/event in Posthog
+const { posthog } = usePostHog()
+
+if (posthog) {
+  router.beforeEach((to) => {
+    posthog.capture('$pageview', { path: to.fullPath })
+  })
+}
 
 export default router
