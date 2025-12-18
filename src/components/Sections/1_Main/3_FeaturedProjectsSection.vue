4c083c357130185b60093f202810c06c873faef9
Anne Rodenburg <rodenburg.bi@gmail.com>
2025-12-18 12:39:52 +0100

    Included jbg

--- src/components/Sections/1_Main/3_FeaturedProjectsSection.vue
+++ src/components/Sections/1_Main/3_FeaturedProjectsSection.vue
@@ -4,41 +4,44 @@ const projects = [
     title: 'Data lakehouse development',
     metrics: 'Improved workflow processes by 80%',
     description:
-      'Developed a Gen3 data lakehouse in Databricks using Delta tables, \
-      optimizing Amazon APIs to enhance performance and enable efficient data ingestion.',
+    'Maintained and expanded a databricks data lakehouse that unified raw, semi-structured, and analytics-ready data into a single platform. The solution improved data availability, reliability, and performance enabling faster, more consistent reporting.',
     subdescription:
       'Proof of concept utilizing AEP elements for CI/CD  <a href="https://medium.com/@rodenburg.bi/solving-aep-object-management-in-multiple-sandboxes-through-code-882b23ce5f18" target="_blank" class="text-moss-500 hover:underline">Check out the blog here</a>.',
     technologies: [
       'Databricks',
       'Adobe Experience Platform',
       'Azure Data Factory',
-      'Delta tables',
-      'DevOps',
-      'GIT',
       'Power BI',
-      'API enhancement',
     ],
-    image: '/images/Gen3_data_platform.png',
+    // image: '/images/Gen3_data_platform.png',
+  },
+  {
+    title: 'JBG-advisory',
+    metrics: 'Advisory role in establishing startup-enterprise collaborations', 
+    description:
+      'The consultancy that bridges the gap between established enterprises and startups, helping both navigate complex collaboration challenges. We leverage entrepreneurial experience across sectors.', 
+    subdescription:
+      'Check us out here: <a href="https://jbgai.com/en/" target="_blank" class="text-moss-500 hover:underline">JBG-advisory</a>.',
+    technologies: [
+      'Digital strategy frameworks',
+      'Operational excellence tools',
+    ], 
   },
   {
     title: 'Greenfield Data Warehouse',
     metrics: 'Established the Business Intelligence Department',
     description:
-      'Led the migration of an on-premise data warehouse to Snowflake, whilst \
-    also restructuring the data model to include best practices and data goverance using dbt.',
-    subdescription: 'Implemented automated feedback for CRM tools',
+      'Scaled the BI capability at Solytic, aligning analytics with the needs of stakeholders across commercial, operational, and technical teams. Owned data source integration across multiple domains, and consolidating into governed, Power-BI dashboards ensuring consistent, trusted insights.',
+    // subdescription: 'Implemented automated feedback for CRM tools',
     technologies: [
       'Snowflake',
-      'SQL',
       'dbt',
       'Azure ',
       'Airflow',
-      'GIT',
       'Power BI',
-      'Docker',
     ],
 
-    image: '/images/Gen2_data_platform.png',
+    // image: '/images/Gen2_data_platform.png',
   },
 ];
 </script>
@@ -47,7 +50,7 @@ const projects = [
   <section id="projects" class="pt-20 pb-20 bg-gradient-to-br from-primary-50 to-white dark:from-gray-900 dark:to-gray-800 py-10">
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
       <h2 class="section-title">Featured Projects</h2>
-      <div class="grid md:grid-cols-2 lg:grid-cols-2 gap-8">
+      <div class="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
         <div v-for="(project, index) in projects" :key="index" class="card p-8">
           <h3 class="text-2xl font-semibold mb-3">{{ project.title }}</h3>
           <p class="text-moss-500 font-medium text-lg mb-4">
